7283ca8e0a26667c3b7a92ed9a0ba2d2f53122e7
yeyintthu9 <yeyintthu536@gmail.com>
2025-09-05 10:54:38 +0700

    [update] Change VUs to 30

--- loadtest/k6-script.js
+++ loadtest/k6-script.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-  vus: 25, // virtual users
+  vus: 30, // virtual users
   duration: '30s', // test duration
 };
 
